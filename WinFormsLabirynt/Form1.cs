02e97e769016972cf9978318e1d5d18966e953e8
Krzakerson <92225516+Krzakerson@users.noreply.github.com>
2022-02-17 21:47:46 +0100

    move
    Kulka powraca

--- WinFormsLabirynt/Form1.cs
+++ WinFormsLabirynt/Form1.cs
@@ -15,16 +15,73 @@ namespace WinFormsLabirynt
 
         private void moveTimerEvent(object sender, EventArgs e)
         {
+            if(moveLeft == true && pictureBox1.Left >  0)
+            {
+                pictureBox1.Left -= speed;
+            }
 
+            if (moveRight== true && pictureBox1.Left < 646)
+            {
+                pictureBox1.Left += speed;
+            }
+
+            if (moveUp == true && pictureBox1.Top > 0 )
+            {
+                pictureBox1.Top -= speed;
+            }
+
+
+            if (moveDown == true && pictureBox1.Top < 517)
+            {
+                pictureBox1.Top += speed;
+            }
         }
 
         private void keyisdown(object sender, KeyEventArgs e)
         {
+            if(e.KeyCode == Keys.Left)
+            {
+                moveLeft = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = true;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                moveDown = true;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = true;
+            }
 
         }
 
         private void keyisup(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                moveDown = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = false;
+            }
 
         }
     }
