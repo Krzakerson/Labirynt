120daa354293b30b7fdf1fbbe2827710d6fa3ba9
Krzakerson <92225516+Krzakerson@users.noreply.github.com>
2022-02-23 21:25:57 +0100

    size fixing
    Menu and win card size has been fixed
    next step losing after touching the wall

--- WinFormsLabirynt/Lose.Designer.cs
+++ WinFormsLabirynt/Lose.Designer.cs
@@ -39,29 +39,31 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(295, 26);
+            this.label1.Location = new System.Drawing.Point(258, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(186, 50);
+            this.label1.Size = new System.Drawing.Size(150, 41);
             this.label1.TabIndex = 0;
             this.label1.Text = "Przegrana";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(148, 91);
+            this.pictureBox1.Location = new System.Drawing.Point(93, 72);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(482, 270);
+            this.pictureBox1.Size = new System.Drawing.Size(481, 207);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // Lose
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Lose";
             this.Text = "Lose";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- WinFormsLabirynt/Win.cs
+++ WinFormsLabirynt/Win.cs
@@ -19,7 +19,7 @@ namespace WinFormsLabirynt
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Close();
+            Application.Exit();
         }
     }
 }
